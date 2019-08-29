1d8481189c6fdc97e5990ace169732a93914f1ad
Dylan <Dylan@LAPTOP-HU97QSV9>
2019-08-29 16:08:23 -0400

    We made some changes

--- Program.cs
+++ Program.cs
@@ -7,6 +7,12 @@ namespace HellloISM4300_1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //This is a change with one line comment
+            /*
+              This is a multi line
+              comment
+             * */
+            Console.WriteLine("This is a change.");
         }
     }
 }
